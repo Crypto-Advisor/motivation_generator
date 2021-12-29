0b722e7e80f13943e2b63cbc3b2bb6c292a04355
Crypto-Advisor <40330472+Crypto-Advisor@users.noreply.github.com>
2021-12-29 16:32:21 -0500

    working random quote system

--- script.js
+++ script.js
@@ -2,21 +2,34 @@ console.log('working!')
 
 document.getElementById('submitmsg').onclick = function() {
     console.log('wow')
-    var inputVal = document.getElementById('mainText').value;
+    var inputVal = document.getElementById("mainText").value;
     var ul = document.getElementById('messageList');
     var li = document.createElement('li');
     li.appendChild(document.createTextNode(inputVal));
     ul.appendChild(li);
+    document.getElementById("mainText").value = '';
     quoteMessage();
 };
 
 
 function quoteMessage(){
     console.log("randomquote")
-    var fr = new FileReader();
-    
-    var text = fr.readAsText("./quotes.txt");
 
-    console.log(text)
+    let requestURL = 'https://raw.githubusercontent.com/Crypto-Advisor/motivation_generator/texting_backend/quotes.json';
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function() {
+        var ul = document.getElementById('messageList');
+        var li = document.createElement('li');
+        const quotes = request.response;
+        var random = Math.floor(Math.random() * quotes['quotes'].length)
+        var text = quotes['quotes'][random].text + "\n Written by: " + quotes['quotes'][random].author
+        li.appendChild(document.createTextNode(text));
+        li.setAttribute("class", "quote");
+        ul.appendChild(li);
+    }
+
 
 }
